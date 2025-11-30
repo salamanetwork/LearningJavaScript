211fed13d5a2e36947090f21630beae0dd6de790
salamanetwork <salamanetwork@gmail.com>
2025-11-30 18:00:55 +0400

    Day 01 Overview Modified Day 02 Added

---
+++ day02.js
@@ -0,0 +1,22 @@
+/*
+Day 02 — Real-World Browser Power:
+    01️ - DOM Patterns & Performance (2 hours)
+        01 - event delegation
+        02 - avoiding reflow/repaint
+        03 - template rendering
+    02 - Modular JavaScript & Architecture / No frameworks — just modules (2 hours)
+        01 - separation of concerns
+        02 - reusable components
+    03 - Local Storage + Data Persistence (1 hour)
+        01 - Build a real feature that remembers user actions.
+    04 - Error Handling & Defensive JS (1 hour)
+        01 - try/catch
+        02 - optional chaining
+        03 - default values
+        04 - type guarding
+    05 - Build a Mini Project (3 hours)
+        01 - A real-time character counter
+        02 - A notes app with local storage
+        03 - Shopping cart (no backend, pure JS)
+        04 - Weather app using fetch
+*/
